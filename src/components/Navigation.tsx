5665fa0be8675133b709116440930c2dc3d5f400
SquishyMona <kotonefemc@gmail.com>
2024-03-19 15:02:09 -0400

    fixed sidebar and navbar overlap

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -22,7 +22,7 @@ export default function Navigation() {
   //}, []);
 
   return (
-    <nav className="navbar bg-base-100 sticky top-0 z-10">
+    <nav className="navbar bg-base-100 sticky top-0 md:z-10">
       <div className="navbar-start">
           <button onClick={openDrawer} tabIndex={0} role="button" className="btn btn-ghost btn-circle md:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
